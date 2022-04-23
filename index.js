1e75616e4a97f834e76ad165c8bc6c12a2c4d54f
Nela Letochová <nela.letochova.wxc@gmail.com>
2022-04-23 23:07:18 +0200

    Doladění JS

--- index.js
+++ index.js
@@ -1,18 +1,16 @@
 let playerOnTurn = 'circle';
+const playerCrossElm = `<p class="game__player--actual">Hraje:</p><img class="game__icons" src="podklady/cross.svg" alt="Malý bílý křížek" />`;
+const playerCircleElm = `<p class="game__player--actual">Hraje:</p><img class="game__icons" src="podklady/circle.svg" alt="Malé bílé kolečko" />`;
 
 const ticTac = (event) => {
   if (playerOnTurn === 'circle') {
     event.target.classList.add('area__field--circle');
-    document.querySelector(
-      '.game__player',
-    ).innerHTML = `<p class="game__player--actual">Hraje:</p><img class="game__icons" src="podklady/cross.svg" alt="Malý bílý křížek" />`;
+    document.querySelector('.game__player').innerHTML = playerCrossElm;
     playerOnTurn = 'cross';
     event.target.disabled = true;
   } else if (playerOnTurn === 'cross') {
     event.target.classList.add('area__field--cross');
-    document.querySelector(
-      '.game__player',
-    ).innerHTML = `<p class="game__player--actual">Hraje:</p><img class="game__icons" src="podklady/circle.svg" alt="Malé bílé kolečko" />`;
+    document.querySelector('.game__player').innerHTML = playerCircleElm;
     playerOnTurn = 'circle';
     event.target.disabled = true;
   }
